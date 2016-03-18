e979dc7e72c351bade7b67bb3eaeeb7adddc36d0
Antonio Calzada <icalzada@lnphi.com>
2016-03-18 16:14:37 -0600

    Cambios 18 de marzo Se encapsulo todo el código en el archivo multidaterangepicker.js Se corrigieron los archivos de acuerdo a la guía de estilo de código. Se renombro el archivo index.html por en-example.html, también se agrego el ejemplo es-example.html

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Datepicker
 Datepicker para seleccionar día espefifico, rango de fechas y multi-fecha
 
+Multidaterangepicker used bootstrap-datepicker
+
+# Docs
+http://bootstrap-datepicker.readthedocs.org/en/latest/
+
+# OnlineDemo
+http://eternicode.github.io/bootstrap-datepicker/
--- js/multidaterangepicker.js
+++ js/multidaterangepicker.js
@@ -7,31 +7,52 @@
       factory(jQuery);
   }
 }(function($, undefined){
-  var dates = "";
-  var options
-  $.fn.multidaterangepicker = function(options) {
-    this.options = options
+
+  var languages = {
+    en: {
+      btnSingle: "Single Date",
+      btnRange: "Range Date",
+      btnMultiple: "Multiple Date",
+      inputStartDate: "Start Date",
+      inputEndDate: "End Date",
+      btnAccept: "Accept"
+    },
+    es: {
+      btnSingle: "Fecha",
+      btnRange: "Rango de fechas",
+      btnMultiple: "Fechas multiples",
+      inputStartDate: "Fecha inicio",
+      inputEndDate: "Fecha fin",
+      btnAccept: "Aceptar"
+    }
   };
 
-  var multidaterangepickerElement = function(){
+  var dates = [];
+  var settings;
+  var exists = false;
+  var show = false;
+  var div;
+  var button = 1;
+  
+  var startRange = false;
+  var actualizar = false;
 
-    var divOptions = $('<div class="mdpr-options"></div>')
-    var btnSingle = $('<button id="mdpr-btnSingle" class="mdpr-btn" >Single Date</button>');
-    var btnRange = $('<button id="mdpr-btnRange" class="mdpr-btn">Range Date</button>');
-    var btnMultiple = $('<button id="mdpr-btnMultiple" class="mdpr-btn">Multiple Dates</button>');
-    var inputStartDate = $('<input type="button" id="mdpr-startDate" class="mdpr-range mdpr-form-control" placeholder="Start Date"></label>');
-    var inputEndDate = $('<input type="button" id="mdpr-endDate" class="mdpr-range mdpr-form-control" placeholder="End Date"></label>');
-    var btnAccept = $('<button class="mdpr-btn mdpr-accept" id="mdpr-btnAccept">Accept</button>');
-    var divSingle = $('<div id="mdpr-container-single"></div>');
-    var divRange = $('<div id="mdpr-container-range"></div>');
-    var divMultiple = $('<div id="mdpr-container-multiple"></div>');
-    
-    var dates = "";
-    var button = 1;
-    
-    var modificarRange = false;
+  var divMdpr = $('<div id="multidaterangepicker" class="mdpr-dropdown"></div>');
+  var divOptions = $('<div class="mdpr-options"></div>');
+  var btnSingle = $('<button id="mdpr-btnSingle" class="mdpr-btn" ></button>');
+  var btnRange = $('<button id="mdpr-btnRange" class="mdpr-btn"></button>');
+  var btnMultiple = $('<button id="mdpr-btnMultiple" class="mdpr-btn"></button>');
+  var inputStartDate = $('<input type="button" id="mdpr-startDate" class="mdpr-range mdpr-form-control" value=""></label>');
+  var inputEndDate = $('<input type="button" id="mdpr-endDate" class="mdpr-range mdpr-form-control" value=""></label>');
+  var btnAccept = $('<button class="mdpr-btn mdpr-accept" id="mdpr-btnAccept"></button>');
+  var divSingle = $('<div id="mdpr-container-single"></div>');
+  var divRange = $('<div id="mdpr-container-range"></div>');
+  var divMultiple = $('<div id="mdpr-container-multiple"></div>');
 
-    var settings = $.extend({
+  $.fn.multidaterangepicker = function(options) {
+    div = this;
+    div.on("click",clickDate);
+    settings = $.extend({
       autoclose: false,
       beforeShowDay: $.noop,
       beforeShowMonth: $.noop,
@@ -49,6 +70,7 @@
       language: 'en',
       minViewMode: 0,
       maxViewMode: 2,
+      multidateSeparator: ",",
       orientation: "auto",
       rtl: false,
       startDate: -Infinity,
@@ -64,232 +86,241 @@
       immediateUpdates: false,
       title: ''
     }, options);
+  };
 
-    divOptions.append(btnSingle);
-    divOptions.append(btnRange);
-    divOptions.append(btnMultiple);
-    divOptions.append(inputStartDate);
-    divOptions.append(inputEndDate);
-    divOptions.append(btnAccept);
-    this.append(divOptions);
-    this.append(divSingle);
-    this.append(divRange);
-    this.append(divMultiple);
 
-    btnSingle.click(function(){
-      button = 1;
-      $(this).addClass("mdpr-active");
-      btnRange.removeClass("mdpr-active");
-      btnMultiple.removeClass("mdpr-active");
-      btnAccept.hide();
-      inputStartDate.hide();
-      inputEndDate.hide();
-      divRange.hide();
-      divMultiple.hide();
-      divSingle.show();
-      divSingle.datepicker({
-        autoclose: settings.autoclose,
-        beforeShowDay: settings.beforeShowDay,
-        beforeShowMonth: settings.beforeShowMonth,
-        beforeShowYear: settings.beforeShowYear,
-        calendarWeeks: settings.calendarWeeks,
-        clearBtn: settings.clearBtn,
-        toggleActive: settings.toggleActive,
-        daysOfWeekDisabled: settings.daysOfWeekDisabled,
-        daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
-        datesDisabled: settings.datesDisabled,
-        endDate: settings.endDate,
-        forceParse: settings.forceParse,
-        format: settings.format,
-        keyboardNavigation: settings.keyboardNavigation,
-        language: settings.language,
-        minViewMode: settings.minViewMode,
-        maxViewMode: settings.maxViewMode,
-        orientation: settings.orientation,
-        rtl: settings.rtl,
-        startDate: settings.startDate,
-        startView: settings.startView,
-        todayBtn: settings.todayBtn,
-        todayHighlight: settings.todayHighlight,
-        weekStart: settings.weekStart,
-        disableTouchKeyboard: settings.disableTouchKeyboard,
-        enableOnReadonly: settings.enableOnReadonly,
-        showOnFocus: settings.showOnFocus,
-        zIndexOffset: settings.zIndexOffset,
-        container: settings.container,
-        immediateUpdates: settings.immediateUpdates,
-        title: settings.title
-      });
+  btnSingle.click(function(){
+    button = 1;
+    $(this).addClass("mdpr-active");
+    btnRange.removeClass("mdpr-active");
+    btnMultiple.removeClass("mdpr-active");
+    btnAccept.hide();
+    inputStartDate.hide();
+    inputEndDate.hide();
+    divRange.hide();
+    divMultiple.hide();
+    divSingle.show();
+    divSingle.datepicker({
+      autoclose: settings.autoclose,
+      beforeShowDay: settings.beforeShowDay,
+      beforeShowMonth: settings.beforeShowMonth,
+      beforeShowYear: settings.beforeShowYear,
+      calendarWeeks: settings.calendarWeeks,
+      clearBtn: settings.clearBtn,
+      toggleActive: settings.toggleActive,
+      daysOfWeekDisabled: settings.daysOfWeekDisabled,
+      daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
+      datesDisabled: settings.datesDisabled,
+      endDate: settings.endDate,
+      forceParse: settings.forceParse,
+      format: settings.format,
+      keyboardNavigation: settings.keyboardNavigation,
+      language: settings.language,
+      minViewMode: settings.minViewMode,
+      maxViewMode: settings.maxViewMode,
+      multidateSeparator: settings.multidateSeparator,
+      orientation: settings.orientation,
+      rtl: settings.rtl,
+      startDate: settings.startDate,
+      startView: settings.startView,
+      todayBtn: settings.todayBtn,
+      todayHighlight: settings.todayHighlight,
+      weekStart: settings.weekStart,
+      disableTouchKeyboard: settings.disableTouchKeyboard,
+      enableOnReadonly: settings.enableOnReadonly,
+      showOnFocus: settings.showOnFocus,
+      zIndexOffset: settings.zIndexOffset,
+      container: settings.container,
+      immediateUpdates: settings.immediateUpdates,
+      title: settings.title
     });
+  });
 
-    btnRange.click(function() {
-      button = 2;
-      $(this).addClass("mdpr-active");
-      btnSingle.removeClass("mdpr-active");
-      btnMultiple.removeClass("mdpr-active");
-      btnAccept.show();
-      divSingle.hide();
-      divMultiple.hide();
-      divRange.show();
-      divRange.datepicker({
-        autoclose: settings.autoclose,
-        beforeShowDay: settings.beforeShowDay,
-        beforeShowMonth: settings.beforeShowMonth,
-        beforeShowYear: settings.beforeShowYear,
-        calendarWeeks: settings.calendarWeeks,
-        clearBtn: settings.clearBtn,
-        toggleActive: settings.toggleActive,
-        daysOfWeekDisabled: settings.daysOfWeekDisabled,
-        daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
-        datesDisabled: settings.datesDisabled,
-        endDate: settings.endDate,
-        forceParse: settings.forceParse,
-        format: settings.format,
-        keyboardNavigation: settings.keyboardNavigation,
-        language: settings.language,
-        minViewMode: settings.minViewMode,
-        maxViewMode: settings.maxViewMode,
-        orientation: settings.orientation,
-        rtl: settings.rtl,
-        startDate: settings.startDate,
-        startView: settings.startView,
-        todayBtn: settings.todayBtn,
-        todayHighlight: settings.todayHighlight,
-        weekStart: settings.weekStart,
-        disableTouchKeyboard: settings.disableTouchKeyboard,
-        enableOnReadonly: settings.enableOnReadonly,
-        showOnFocus: settings.showOnFocus,
-        zIndexOffset: settings.zIndexOffset,
-        container: settings.container,
-        immediateUpdates: settings.immediateUpdates,
-        title: settings.title,
-        inputs: $('.mdpr-range')
-      });
-      inputStartDate.show();
-      inputEndDate.show()
-      if(modificarRange){
-        inputStartDate.focus();
+  btnRange.click(function() {
+    button = 2;
+    $(this).addClass("mdpr-active");
+    btnSingle.removeClass("mdpr-active");
+    btnMultiple.removeClass("mdpr-active");
+    btnAccept.hide();
+    divSingle.hide();
+    divMultiple.hide();
+    divRange.show();
+    divRange.datepicker({
+      autoclose: settings.autoclose,
+      beforeShowDay: settings.beforeShowDay,
+      beforeShowMonth: settings.beforeShowMonth,
+      beforeShowYear: settings.beforeShowYear,
+      calendarWeeks: settings.calendarWeeks,
+      clearBtn: settings.clearBtn,
+      toggleActive: settings.toggleActive,
+      daysOfWeekDisabled: settings.daysOfWeekDisabled,
+      daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
+      datesDisabled: settings.datesDisabled,
+      endDate: settings.endDate,
+      forceParse: settings.forceParse,
+      format: settings.format,
+      keyboardNavigation: settings.keyboardNavigation,
+      language: settings.language,
+      minViewMode: settings.minViewMode,
+      maxViewMode: settings.maxViewMode,
+      multidate: true,
+      multidateSeparator: settings.multidateSeparator,
+      orientation: settings.orientation,
+      rtl: settings.rtl,
+      startDate: settings.startDate,
+      startView: settings.startView,
+      todayBtn: settings.todayBtn,
+      todayHighlight: settings.todayHighlight,
+      weekStart: settings.weekStart,
+      disableTouchKeyboard: settings.disableTouchKeyboard,
+      enableOnReadonly: settings.enableOnReadonly,
+      showOnFocus: settings.showOnFocus,
+      zIndexOffset: settings.zIndexOffset,
+      container: settings.container,
+      immediateUpdates: settings.immediateUpdates,
+      title: settings.title
+    });
+    inputStartDate.show();
+    inputEndDate.show();
+    inputStartDate.addClass("mdpr-active-form-control");
+  });
+  
+  btnMultiple.click(function(){
+    button = 3;
+    $(this).addClass("mdpr-active");
+    btnSingle.removeClass("mdpr-active");
+    btnRange.removeClass("mdpr-active");
+    btnAccept.show();
+    inputStartDate.hide();
+    inputEndDate.hide();
+    divSingle.hide();
+    divRange.hide();
+    divMultiple.show();
+    divMultiple.datepicker({
+      autoclose: settings.autoclose,
+      beforeShowDay: settings.beforeShowDay,
+      beforeShowMonth: settings.beforeShowMonth,
+      beforeShowYear: settings.beforeShowYear,
+      calendarWeeks: settings.calendarWeeks,
+      clearBtn: settings.clearBtn,
+      toggleActive: settings.toggleActive,
+      daysOfWeekDisabled: settings.daysOfWeekDisabled,
+      daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
+      datesDisabled: settings.datesDisabled,
+      endDate: settings.endDate,
+      forceParse: settings.forceParse,
+      format: settings.format,
+      keyboardNavigation: settings.keyboardNavigation,
+      language: settings.language,
+      minViewMode: settings.minViewMode,
+      maxViewMode: settings.maxViewMode,
+      multidate: true,
+      multidateSeparator: settings.multidateSeparator,
+      orientation: settings.orientation,
+      rtl: settings.rtl,
+      startDate: settings.startDate,
+      startView: settings.startView,
+      todayBtn: settings.todayBtn,
+      todayHighlight: settings.todayHighlight,
+      weekStart: settings.weekStart,
+      disableTouchKeyboard: settings.disableTouchKeyboard,
+      enableOnReadonly: settings.enableOnReadonly,
+      showOnFocus: settings.showOnFocus,
+      zIndexOffset: settings.zIndexOffset,
+      container: settings.container,
+      immediateUpdates: settings.immediateUpdates,
+      title: settings.title
+    });
+  });
+
+  divSingle.on("changeDate", function(e) {
+    dates = $(this).datepicker('getFormattedDate');
+    divMdpr.hide();
+    show = false;
+    var datesArray = new Array();
+    datesArray.dates = dates.split(settings.multidateSeparator);
+    datesArray.option = "single";
+    div.trigger("getDates",[datesArray]);
+    $("#resultDates").remove();
+    spanDate = $('<span id="resultDates"></span>');
+    spanDate.append(dates);
+    div.append(spanDate);
+  });
+
+  divRange.on("changeDate", function(e) {
+    dates = $(this).datepicker('getFormattedDate');
+    if(!actualizar){
+      if(!startRange){
+        startRange = true;
+        inputStartDate.removeClass('mdpr-active-form-control');
+        inputEndDate.addClass('mdpr-active-form-control');
+        divRange.datepicker('setStartDate', dates);
+        actualizar = true;
+        divRange.datepicker('setDate', dates);
       }else{
-        inputEndDate.focus();
+        startRange = false;
+        divMdpr.hide();
+        show = false;
+        inputEndDate.removeClass('mdpr-active-form-control');
+        inputStartDate.addClass('mdpr-active-form-control');
+        var datesArray = new Array();
+        datesArray.dates = dates.split(settings.multidateSeparator);
+        datesArray.option = "range";
+        div.trigger("getDates",[datesArray]);
+        $("#resultDates").remove();
+        spanDate = $('<span id="resultDates"></span>');
+        spanDate.append(datesArray.dates[0] + " - " + datesArray.dates[1]);
+        div.append(spanDate);
+        actualizar = true;
+        divRange.datepicker('clearDates');
+        divRange.datepicker('setStartDate',settings.startDate);
       }
-      
-    });
-    
-    btnMultiple.click(function(){
-      button = 3;
-      $(this).addClass("mdpr-active");
-      btnSingle.removeClass("mdpr-active");
-      btnRange.removeClass("mdpr-active");
-      btnAccept.show();
-      inputStartDate.hide();
-      inputEndDate.hide();
-      divSingle.hide();
-      divRange.hide();
-      divMultiple.show();
-      divMultiple.datepicker({
-        autoclose: settings.autoclose,
-        beforeShowDay: settings.beforeShowDay,
-        beforeShowMonth: settings.beforeShowMonth,
-        beforeShowYear: settings.beforeShowYear,
-        calendarWeeks: settings.calendarWeeks,
-        clearBtn: settings.clearBtn,
-        toggleActive: settings.toggleActive,
-        daysOfWeekDisabled: settings.daysOfWeekDisabled,
-        daysOfWeekHighlighted: settings.daysOfWeekHighlighted,
-        datesDisabled: settings.datesDisabled,
-        endDate: settings.endDate,
-        forceParse: settings.forceParse,
-        format: settings.format,
-        keyboardNavigation: settings.keyboardNavigation,
-        language: settings.language,
-        minViewMode: settings.minViewMode,
-        maxViewMode: settings.maxViewMode,
-        multidate: true,
-        orientation: settings.orientation,
-        rtl: settings.rtl,
-        startDate: settings.startDate,
-        startView: settings.startView,
-        todayBtn: settings.todayBtn,
-        todayHighlight: settings.todayHighlight,
-        weekStart: settings.weekStart,
-        disableTouchKeyboard: settings.disableTouchKeyboard,
-        enableOnReadonly: settings.enableOnReadonly,
-        showOnFocus: settings.showOnFocus,
-        zIndexOffset: settings.zIndexOffset,
-        container: settings.container,
-        immediateUpdates: settings.immediateUpdates,
-        title: settings.title
-      });
-    });
-
-    var changeDate = function () {
-     
+    }else{
+      actualizar = false;
     }
+  });
 
-    divSingle.on("changeDate", function(e) {
-      $(this).parent().removeClass("mdpr-dropdown");
-      $(this).parent().html("");
-      $(this).trigger("getDates",e);
-    });
+  divMultiple.on("changeDate", function(e) {
+    dates = $(this).datepicker('getFormattedDate');
+  });
 
-    divMultiple.on("changeDate", function(e) {
-      $(this).trigger("getDates",e);
-    });
+  btnAccept.on("click",function(){
+    divMdpr.hide();
+    show = false;
+    var datesArray = new Array();
+    datesArray.dates = dates.split(settings.multidateSeparator);
+    datesArray.option = "multiple";
+    div.trigger("getDates",[datesArray]);
+    $("#resultDates").remove();
+    spanDate = $('<span id="resultDates">Multiple</span>');
+    div.append(spanDate);
+  });
 
-    btnSingle.trigger("click");
-
-    this.addClass("mdpr-dropdown");
-    return this;
+  function clickDate(){
+    if(!exists && !show){
+      btnSingle.append(languages[settings.language].btnSingle);
+      btnRange.append(languages[settings.language].btnRange);
+      btnMultiple.append(languages[settings.language].btnMultiple);
+      inputStartDate.val(languages[settings.language].inputStartDate);
+      inputEndDate.val(languages[settings.language].inputEndDate);
+      btnAccept.append(languages[settings.language].btnAccept);
+      divOptions.append(btnSingle);
+      divOptions.append(btnRange);
+      divOptions.append(btnMultiple);
+      divOptions.append(inputStartDate);
+      divOptions.append(inputEndDate);
+      divOptions.append(btnAccept);
+      divMdpr.append(divOptions);
+      divMdpr.append(divSingle);
+      divMdpr.append(divRange);
+      divMdpr.append(divMultiple);
+      $("body").append(divMdpr);
+      exists = true;
+      show = true;
+      btnSingle.trigger("click");
+    }else if(!show){
+      divMdpr.show();
+    }
   }
 
-  $(this).on("click",function(){
-    return multidaterangepickerElement();
-    console.log("no");
-  })
-
-
-
 }));
-
-	/*
-	
-	$("#date").click(function(){
-		$("#calendario").show();
-		if(button==1){
-			$("#btnEspecifica").trigger("click");	
-		}else if(button==2){
-			modificarRange = $("#fechaFin").val() != "" ? true : false;
-			$("#btnRango").trigger("click");
-		}else{
-			$("#btnMultiple").trigger("click");
-		}
-	});
-	
-	$('#sandbox-container1').on('changeDate', function(e) {
-		dates = $(this).datepicker('getFormattedDate')
-		$("#date").html("<span>Date</span> - "+ dates +" <b class="caret"></b>");
-		$("#calendario").hide();
-	});
-	
-	$('#sandbox-container3').on('changeDate', function(e) {
-		dates = $(this).datepicker('getFormattedDate')
-	});
-	
-	$("#btnAceptar").click(function(){
-		if(button==2){
-			$("#date").html("<span>Date</span> - " + $("#fechaInicio").val() + " - " + $("#fechaFin").val() + " <b class="caret"></b>");
-			dates = "";
-			dates[0] = $("#fechaInicio").val();
-			dates[1] = $("#fechaFin").val();
-		}else{
-			$("#date").html("<span>Date</span> - Fechas multiples <b class="caret"></b>");
-		}
-		$("#calendario").hide();
-	});
-	
-	$("#fechaInicio").change(function(){
-		if(modificarRange){
-			$("#fechaFin").focus();	
-		}
-	});
-*/
